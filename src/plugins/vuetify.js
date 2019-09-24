08c0152eece8033d670d6d93037963b95abe3c28
Istvan Rolland Orgonas <rorgonas@gmail.com>
2019-09-23 22:25:38 -0400

    add - material icon module

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,11 +1,11 @@
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-// import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   icons: {
-    iconfont: 'mdi',
+    iconfont: 'md',
   },
 });
